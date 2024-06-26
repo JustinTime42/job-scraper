919ec4b3665861e890c09a0cab6c627e6cf1eb8a
JustinTime42 <justincase18@msn.com>
2024-06-25 16:58:24 -0800

    added best guess careers page urls to the results

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import csv
 
 visited_urls = []
+likely_careers_page = ""
 def extract_page_info(url):
     """
     Extracts metadata (like title and description), headings, text,
@@ -28,8 +29,20 @@ def extract_page_info(url):
     metadata = {}
     metadata['title'] = extract_title(soup)
     metadata['description'] = extract_description(soup)
-    email_addresses = scrape_links_and_emails(soup, url, [])
-    print(f"Email addresses found: {email_addresses}")
+    email_addresses = []
+    careers_page = []
+    site_map = create_site_map(soup, url, [])
+
+    print(f"site map length: {len(site_map)}")
+    for site in site_map:
+        print(f"Scraping {site}")
+        new_emails = scrape_emails(site)
+        for email in new_emails:
+            if email not in email_addresses:
+                email_addresses.append(email.strip())
+        if (site.lower().find("career") != -1) or (site.find("job") != -1) or (site.find("work-with-us") != -1) or (site.find("join-us") != -1):
+            print(f"Found careers page: {site}")
+            careers_page.append(site)
 
     # Prepare data for CSV writing
     page_data = [
@@ -37,13 +50,14 @@ def extract_page_info(url):
             "url": url,
             "title": metadata['title'],
             "description": metadata['description'],
-            "email_addresses": ",".join(email_addresses) if email_addresses else ""
+            "email_addresses": ",".join(email_addresses) if email_addresses else "",
+            "careers_page": ",".join(careers_page) if careers_page else ""
         }
     ]
 
     # Open or create CSV file
     with open('output.csv', 'a', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ["url", "title", "description", "email_addresses"]
+        fieldnames = ["url", "title", "description", "email_addresses", "careers_page"]
         csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         # Write header to CSV file
@@ -89,62 +103,74 @@ def extract_emails_from_html(soup):
 
     return emails
 
-def scrape_links_and_emails(soup, base_url, emails_found):
-
-    
+def create_site_map(soup, base_url, site_urls):
     # Get links in the current page
     for link in soup.find_all('a', href=True):
+           
         full_link = f"http://{base_url}/{link.get('href')}" if not link.get('href').startswith("http") else link.get('href')
-        request_headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
+        
         if (get_domain(full_link) == get_domain(base_url)) and (full_link not in visited_urls):
             visited_urls.append(full_link)
+            site_urls.append(full_link)
             try:
-                response = requests.get(full_link, headers=request_headers)
-                print(f"{full_link}")
+                response = requests.get(full_link)
                 if response.status_code == 200:
                     # Get BeautifulSoup object for the response
                     soup_subpage = BeautifulSoup(response.text, 'html.parser')
-
-                    # Recursively find emails in this page and links in this page that go back to the same domain.
-                    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-                    new_emails = re.findall(email_pattern, soup_subpage.get_text())
-                    more_emails = extract_emails_from_html(soup_subpage)
-                    for email in new_emails + more_emails:
-                        if email not in emails_found:
-                            emails_found.append(email.strip())
                     # Recursively check links within this page
                     for link in soup_subpage.find_all('a', href=True):                        
-                            if not link.get('href').startswith("http"):
-                                full_link = f"http://{base_url}/{link.get('href')}"
-                            else:
-                                link.get('href')
-                            if get_domain(full_link) == get_domain(base_url):                                    
-                                    if full_link not in visited_urls:                                        
-                                        scrape_links_and_emails(soup_subpage, full_link, emails_found)                           
+                        if not link.get('href').startswith("http"):
+                            full_link = f"http://{base_url}/{link.get('href')}"
+                        else:
+                            link.get('href')
+                        if get_domain(full_link) == get_domain(base_url):                                    
+                            if full_link not in visited_urls:                                    
+                                create_site_map(soup_subpage, full_link, site_urls) 
             except Exception as e:
                 print(f"Failed to access {full_link}: {str(e)}")
                 continue
+    return site_urls
+
+def scrape_emails(site):
+    emails_found = []
+    request_headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }           
+    try:
+        response = requests.get(site, headers=request_headers)
+        if response.status_code == 200:
+            soup_subpage = BeautifulSoup(response.text, 'html.parser')
+            email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            new_emails = re.findall(email_pattern, soup_subpage.get_text())
+            more_emails = extract_emails_from_html(soup_subpage)
+            for email in new_emails + more_emails:
+                if email not in emails_found:
+                    emails_found.append(email.strip())                              
+    except Exception as e:
+        print(f"Failed to access {site}: {str(e)}")
+        return []        
     return emails_found
-
-def find_emails_on_website(base_url):
-    response = requests.get(base_url)
-
-    if response.status_code != 200:
-        print("Failed to access the website.")
-        return []
-
-    soup_main_page = BeautifulSoup(response.text, 'html.parser')
-
-    # Scrape emails from main page and links within it that lead to same domain
-    return scrape_links_and_emails(soup_main_page, base_url, [], 10)
-
 
 if __name__ == "__main__":
     urls = [
         "https://summitdaymedia.com/",
         "https://www.msialaska.com/",
+        "https://wostmann.com/",
+        "https://www.7oaksgroup.com/",
+        "https://www.flyntlok.com/careers/",
+        "https://kartorium.com",
+        "https://wostmann.com/",
+        "https://queryon.com/",
+        "https://nwds-ak.com",
+        "https://webbres.com/",
+        "https://www.nexusdatasolutions.com/",
+        "https://designori.net/",
+        "https://www.7oaksgroup.com/",
+        "https://grio.com/",
+        "https://www.ctg.com",
+        "https://www.ripcord.com/",
+
+
         
         ]  # Replace with the actual URL
     # extract_page_info(url)
